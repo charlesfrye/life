cf29487ed104db3c43d7e50714994f622fb744da
charlesfrye <cfrye59@gmail.com>
2022-01-27 21:11:55 -0800

    including pkg in static files

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { Universe, Cell } from "life";
-import { memory } from "life/life_bg";
+import { Universe, Cell } from "./pkg/life";
+import { memory } from ".pkg/life_bg";
 
 const CELL_SIZE = 5; // px
 const GRID_COLOR = "#CCCCCC";
